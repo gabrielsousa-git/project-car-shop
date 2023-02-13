a691f913a98981a3ec82782b17ab1b1360b60b31
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2023-02-13 18:35:17 -0300

    :sparkles: feat: Req 01 - Crie a rota /cars onde seja possível cadastrar um carro

---
+++ src/Domains/Car.ts
@@ -0,0 +1,38 @@
+import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
+
+export default class Car extends Vehicle {
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(
+    { id,
+      model,
+      year,
+      color,
+      status,
+      buyValue,
+      doorsQty,
+      seatsQty }: ICar,
+  ) {
+    super({ id, model, year, color, status, buyValue });
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
+
+  public setDoorsQty(doorsQty: number) {
+    this.doorsQty = doorsQty;
+  }
+
+  public getDoorsQty() {
+    return this.doorsQty;
+  }
+
+  public setSeatsQty(seatsQty: number) {
+    this.seatsQty = seatsQty;
+  }
+
+  public getSeatsQty() {
+    return this.seatsQty;
+  }
+}
